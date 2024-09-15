5d787e7e70951cfec4d20f5566b8f09136776893
Raijam13 <bustiosterresabraham@gmail.com>
2024-09-15 11:01:52 -0500

    Login formulario

---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,29 @@
+'use client'
+import Styles from './page.module.css'
+
+
+const login = () =>{
+    return (
+        <div className={Styles.render}>
+            
+            <div className={Styles.containerl}>
+            
+                <div className={Styles.formulario}>
+                    <h2>Bienvenido!</h2>
+                    <p>Ingresa tus credenciales para iniciar sesión</p>
+                </div>
+            </div>
+
+            <div className={Styles.containerr}>
+                    d
+            </div>
+        
+
+        </div>
+        );
+    
+}
+
+export default login;
+   
+  
