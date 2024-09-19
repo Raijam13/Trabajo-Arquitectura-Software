dcc5b8c72ecc6176150ed173bb8b1109ec871758
Raijam13 <bustiosterresabraham@gmail.com>
2024-09-19 13:55:33 -0500

    Form login

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,23 +1,51 @@
 'use client'
 import Styles from './page.module.css';
-import "@nextui-org/react/styles.css";
-import {Input} from "@nextui-org/react";
+import 'bootstrap/dist/css/bootstrap.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 const login = () =>{
     return (
         <div className={Styles.render}>
+            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
             
             <div className={Styles.containerl}>
             
                 <div className={Styles.formulario}>
                     <h2>Bienvenido!</h2>
                     <p>Ingresa tus credenciales para iniciar sesión</p>
+                  
+                                    <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Correo</Form.Label>
+                        <Form.Control type="email" placeholder="Ingrese su correo" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
 
-                    <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                    <Input type="email" label="Email" />
-                     <Input type="email" label="Email" placeholder="Enter your email" />
-                        </div>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+
+                    <div className="d-grid gap-2">
+                        <Button variant="primary" size="lg">
+                            Iniciar sesión
+                        </Button>
+                    </div>
+                    </Form>
+
+                
+
+
+                    
                 </div>
             </div>
 
