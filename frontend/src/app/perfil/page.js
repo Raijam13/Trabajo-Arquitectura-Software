aa79af33bf05972b85d4d4e57517dfbe088a66b1
JanoGomezL <20190857@aloe.ulima.edu.pe>
2024-09-21 15:44:02 -0500

    intento

--- frontend/src/app/perfil/page.js
+++ frontend/src/app/perfil/page.js
@@ -8,7 +8,13 @@ const perfil = () =>{
             
         <div className={Styles.barra}>
 
-            <div className={Styles.logo}></div>
+            <div className={Styles.logo}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-briefcase-fill" viewBox="0 0 16 16">
+  <path d="M6.271 1.545c.17-.05.354.025.515.125v2.15c-.07.04-.146.08-.22.121v.38a.5.5 0 0 0 .5.5h1.387c.297-.152.614-.501.883-.917l1.085-2.272A1.25 1.25 0 0 0 9.921 1.545h-3.642zm1.5 0V3.85c.217-.119.433-.208.65-.269v-.848a.5.5 0 0 0-.5-.5h-1.27c-.314.074-.61.246-.864.511V1.545zm1.82.5c-.179.09-.375.217-.58.372v3.888c.083.074.173.14.27.195v.38a.5.5 0 0 0 .5.5h1.645c.314-.074.61-.246.864-.511V2.045zm-2.18 10.224a1.25 1.25 0 0 0 1.25 1.25h3.642c.66 0 1.23-.568 1.23-1.25v-3.643c0-.66-.568-1.23-1.23-1.23h-3.642a1.25 1.25 0 0 0-1.25 1.25v3.643z"/>
+</svg>
+                Freelancers
+                Peru
+            </div>
             <div className={Styles.container}>
                 <div className={Styles.botonesbr}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
@@ -17,16 +23,124 @@ const perfil = () =>{
                     Home
                     
                 </div>
+                <div className={Styles.botonesbr}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+</svg>
+                    Perfil
+                    
+                </div>
+                <div className={Styles.botonesbr}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-briefcase" viewBox="0 0 16 16">
+  <path d="M6.271 1.545c.17-.05.354.025.515.125v2.15c-.07.04-.146.08-.22.121v.38a.5.5 0 0 0 .5.5h1.387c.297-.152.614-.501.883-.917l1.085-2.272A1.25 1.25 0 0 0 9.921 1.545h-3.642zm1.5 0V3.85c.217-.119.433-.208.65-.269v-.848a.5.5 0 0 0-.5-.5h-1.27c-.314.074-.61.246-.864.511V1.545zm1.82.5c-.179.09-.375.217-.58.372v3.888c.083.074.173.14.27.195v.38a.5.5 0 0 0 .5.5h1.645c.314-.074.61-.246.864-.511V2.045zm-2.18 10.224a1.25 1.25 0 0 0 1.25 1.25h3.642c.66 0 1.23-.568 1.23-1.25v-3.643c0-.66-.568-1.23-1.23-1.23h-3.642a1.25 1.25 0 0 0-1.25 1.25v3.643z"/>
+</svg>
+                    Trabajos
+                    
+                </div>
+                <div className={Styles.botonesbr}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+  <path d="M5.5 7a.5.5 0 0 1 .5.5v1.5H14a.5.5 0 0 1 0 1H5.5v1.5a.5.5 0 0 1-1 0V8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9z"/>
+  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
+</svg>
+                    Mis Postulaciones
+                    
+                </div>
+                <div className={Styles.botonesbr}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-task" viewBox="0 0 16 16">
+  <path d="M2 2.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-1z"/>
+  <path d="M3 8a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H3.5a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H3.5a.5.5 0 0 1-.5-.5z"/>
+</svg>
+                    Historial
+                    
+                </div>
 
             </div>
                 
         </div>
+        <div className={Styles.perfil}>
+            <div className={Styles.perfilcontainer}>
+                <div className={Styles.imagenperfil}>
+                    <img src="https://www.pexels.com/photo/woman-wearing-pink-hoodie-10482066/" alt="mujer"/>
+                </div>
+                <div className={Styles.nombres}>
+                    <div className={Styles.nombre}>
+                        Elza Pato
+                    </div>
+                    <div className={Styles.puntuacion}>
+                        4.8
+                    </div>
+                </div>
+            </div>
+
+            <div className={Styles.infopersonal}>
+                <div className={Styles.titulo}>
+                    Acerca de
+                </div>
+                <div className={Styles.infopersonalcontainer}>
+                    <div className={Styles.icono}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+</svg>
+                    </div>
+                    <div className={Styles.info}>
+                        Femenino
+                    </div>
+                </div>
+                <div className={Styles.infopersonalcontainer}>
+                    <div className={Styles.icono}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cake-fill" viewBox="0 0 16 16">
+  <path d="M7 4.029h2.283C9.353 2.11 8.8 1.5 8 1.5c-1.4 0-2.717.418-3.642 1.279h2.014A.5.5 0 0 0 7 4.029zM5 1.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v2.15c-.07.04-.146.08-.22.121v.38a.5.5 0 0 0 .5.5h1.387c.297-.152.614-.501.883-.917l1.085-2.272A1.25 1.25 0 0 0 9.921 1.545h-3.642zm1.5 0V3.85c.217-.119.433-.208.65-.269v-.848a.5.5 0 0 0-.5-.5h-1.27c-.314.074-.61.246-.864.511V1.545zm1.82.5c-.179.09-.375.217-.58.372v3.888c.083.074.173.14.27.195v.38a.5.5 0 0 0 .5.5h1.645c.314-.074.61-.246.864-.511V2.045zm-2.18 10.224a1.25 1.25 0 0 0 1.25 1.25h3.642c.66 0 1.23-.568 1.23-1.25v-3.643c0-.66-.568-1.23-1.23-1.23h-3.642a1.25 1.25 0 0 0-1.25 1.25v3.643z"/>
+</svg>
+                    </div>
+                    <div className={Styles.info}>
+                        Edad 28
+                    </div>
+                </div>
+                <div className={Styles.infopersonalcontainer}>
+                    <div className={Styles.icono}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+  <path d="M8 16s6-5.686 5.108-8c-1.696 2.343-5.108 8-5.108 8zm0-12.965C3.482 3.033 3 3.482 3 8s.482 4.967 1.054 7c.572-2.039 1.872-3.686 3.296-5z"/>
+</svg>
+                    </div>
+                    <div className={Styles.info}>
+                        Lima, Perú
+                    </div>
+                </div>
+            </div>
             
+            <div className={Styles.solicitudes}>
+                <div className={Styles.titulo}>
+                    Solicitudes de trabajo
+                </div>
+                <div className={Styles.solicitudescontainer}>
+                    <div className={Styles.solicitud}>
+                        <div className={Styles.imagenperfil}>
+                            <img src="https://www.pexels.com/photo/portrait-of-smiling-man-with-arms-crossed-4626539/" alt="hombre"/>
+                        </div>
+                        <div className={Styles.nombres}>
+                            <div className={Styles.nombre}>
+                                Aquiles Castro
+                            </div>
+                            <div className={Styles.fecha}>
+                                Hace 3 días
+                            </div>
+                        </div>
+                    </div>
+                    <div className={Styles.imagensolicitud}>
+                        <img src="https://www.pexels.com/photo/red-wooden-door-9500126/" alt="puerta"/>
+                    </div>
+                    <div className={Styles.descripcion}>
+                        Amable y paciente, cumplió con el pago establecido
+                    </div>
+                    <div className={Styles.puntuacion}>
+                        5
+                    </div>
+                </div>
+            </div>
         </div>
-        );
+    </div>
+    );
     
 }
 
 export default perfil;
-   
-  
