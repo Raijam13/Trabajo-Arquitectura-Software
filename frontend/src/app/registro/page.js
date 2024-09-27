fc751d1b543ab7e558340e7d1dfb1ad4dce2c05e
Raijam13 <bustiosterresabraham@gmail.com>
2024-09-27 16:38:34 -0500

    si

--- frontend/src/app/registro/page.js
+++ frontend/src/app/registro/page.js
@@ -14,7 +14,7 @@ const registro = () =>{
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
             <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
             
-            <div className={Styles.containerl}>
+            <div className={Styles.container}>
             
                 <div className={Styles.formulario}>
                     <h2>Unete a nuestra plataforma</h2>
@@ -29,21 +29,21 @@ const registro = () =>{
                         </Form.Text>
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Correo</Form.Label>
-                        <Form.Control type="email"  />
-                        <Form.Text className="text-muted">
-                       
-                        </Form.Text>
-                    </Form.Group>
-
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
                         
                         <Form.Control type="password"  />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                        
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formnombres">
+                        <Form.Label>Nombres</Form.Label>
+                        <Form.Control type="email"  />
+                        <Form.Text className="text-muted">
+                       
+                        </Form.Text>
                     </Form.Group>
 
                     <div className="d-grid gap-2">
@@ -60,18 +60,7 @@ const registro = () =>{
                 </div>
             </div>
 
-            <div className={Styles.containerr}>
-                    <Image
-                    src={ima}
-                    style={{
-                        maxWidth: '200%',
-                        height: 'auto',
-
-                    }}
-                    >
-                        
-                    </Image>
-            </div>
+           
         
 
         </div>
