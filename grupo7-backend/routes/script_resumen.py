37f2b13cb1fb981915df56855483f420819102be
Aron-png <aaronlivias0412@gmail.com>
2024-11-03 17:49:19 -0500

    ganamos

--- grupo7-backend/routes/script_resumen.py
+++ grupo7-backend/routes/script_resumen.py
@@ -4,14 +4,34 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
+#Aun falta un poco más de funcionalidad, por ahora la ID del vendedor es estática.
+url = "http://localhost:3009/comentarios/671ac23dfdd3b7d8c1d73b9a"
+
+def get_todo_comentarios():
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            # Procesar la respuesta
+            return response.json()
+        else:
+            print("Error en la petición GET:", response.status_code)
+            return []
+    except requests.RequestException as e:
+        print("Error en la petición GET:", e)
+        return []
+
+variable = get_todo_comentarios()
+#Extraer solo comentarios
+lista = [item['comentario'] for item in variable if 'comentario' in item]
 
 #Realizar las peticiones get para tener una lista de comentarios
+'''
 lista = ["Recientemente tuve la oportunidad de contratar a Aquiles Hiban para un problema urgente en mi hogar, y la experiencia fue más que satisfactoria. Primero, mi sistema eléctrico comenzó a fallar sin previo aviso, con luces parpadeantes y enchufes que no funcionaban. Lo contacté, y aunque ya era tarde, se mostró disponible para atender mi emergencia en el mismo día. Su conocimiento fue evidente desde el primer momento, y no solo diagnosticó el problema rápidamente, sino que también me explicó con paciencia las posibles causas y las soluciones más adecuadas. Pero su profesionalismo no se quedó ahí. Días después, decidí aprovechar su servicio de cerrajería porque necesitaba cambiar todas las cerraduras de mi casa por razones de seguridad. Me impresionó la rapidez y precisión con la que trabajó. A diferencia de otros profesionales que simplemente cambian la cerradura y se van, Aquiles Hiban me asesoró sobre las opciones más seguras y modernas, y me dejó claro que su prioridad era que yo estuviera 100 porciento satisfecho y seguro. Recomiendo sus servicios a cualquiera que necesite un profesional honesto y altamente capacitado. ¡Excelente trabajo!",
          "Mi experiencia con Aquiles Hiban fue fantástica. Llamé porque necesitaba actualizar el sistema de iluminación de mi negocio, y desde el primer contacto, mostró gran interés en entender mis necesidades y en sugerirme opciones que fueran funcionales y estéticamente agradables. Él se encargó de revisar toda la instalación y de hacer ajustes en el sistema eléctrico para soportar las nuevas luminarias. Lo mejor fue que terminó el trabajo en el plazo acordado y mantuvo una comunicación clara sobre cada paso que iba dando. Me impresionó su nivel de organización y el orden con el que trabaja, algo que considero esencial en un profesional. Posteriormente, tuve problemas con la cerradura de seguridad de una puerta que daba acceso a una zona importante del negocio. Sin dudarlo, le pedí ayuda y su respuesta fue inmediata. No solo resolvió el problema rápidamente, sino que también me enseñó algunos trucos básicos de mantenimiento para evitar futuros inconvenientes. Su honestidad, responsabilidad y conocimientos técnicos me han ganado como cliente fiel, y no dudaré en recomendarlo a cualquier persona que necesite un electricista o cerrajero confiable.",
          "Hace poco, tuve la oportunidad de contratar a Aquiles Hiban para que solucionara varios problemas eléctricos en mi casa, y no puedo estar más satisfecho con su trabajo. Desde el primer momento, demostró profesionalismo, experiencia y un verdadero compromiso con la seguridad y el bienestar de su cliente. Había varios problemas que necesitaban atención urgente: un cortocircuito en la cocina que hacía que se dispararan los fusibles, varios enchufes sueltos que ya no funcionaban y una lámpara que hacía un zumbido extraño al encenderla. En cada uno de estos casos, Aquiles Hiban demostró un profundo conocimiento técnico. Detectó rápidamente la causa de los problemas y me explicó de una forma que pude entender las posibles soluciones. No intentó venderme reparaciones innecesarias y solo propuso lo realmente necesario, lo cual me pareció honesto y profesional.",
          "Aquiles Hiban fue un gran hallazgo, y no puedo estar más agradecido por su profesionalismo. Lo contraté para ayudarme con una serie de reparaciones eléctricas en mi casa, que iban desde revisar enchufes hasta instalar luces y regular el sistema de voltaje para proteger mis dispositivos. Desde que llegó, demostró ser alguien muy atento a los detalles, revisando minuciosamente cada aspecto antes de proceder. Además, respetó todos los protocolos de seguridad, algo que no siempre se ve en otros profesionales, y trabajó con precisión y cuidado en cada tarea. Su experiencia en cerrajería también fue de gran ayuda. Una de las puertas de mi casa necesitaba una cerradura nueva, y no solo trajo una de buena calidad, sino que la instaló de forma que funcionara perfectamente y no ofreciera ningún problema. Su enfoque integral y el interés genuino que mostró en que todo quedara funcionando al 100% me dejaron tranquilo. La combinación de servicios que ofrece es muy conveniente y representa una gran ventaja cuando se busca a alguien confiable. Sin duda, volveré a llamarlo para cualquier otra necesidad que tenga en el futuro. ¡Un profesional excelente!"
         ]
-
+'''
 texto = " ".join(lista)
 
 lista_oraciones = texto.split(".")#Dividir la oracion por puntos
